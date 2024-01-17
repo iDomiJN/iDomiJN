c78e2542f139872bdeca30263aff3c22014ef0a8
iDomiJN <156942098+iDomiJN@users.noreply.github.com>
2024-01-17 12:07:01 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+particlesJS('particles-js', {
+  particles: {
+    number: {
+      value: 80, // Puedes ajustar la cantidad de partículas
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: '#ffffff', // Color de las partículas
+    },
+    shape: {
+      type: 'circle', // Tipo de forma de las partículas
+      stroke: {
+        width: 0,
+        color: '#000000',
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: '#ffffff',
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: 'none',
+      random: false,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: 'canvas',
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'grab',
+      },
+      onclick: {
+        enable: true,
+        mode: 'push',
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 1
